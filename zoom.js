4bd97fd7ed18b0831acc503134eef010b50d5377
TK <155989165+tony-kennah-cloudm@users.noreply.github.com>
2024-02-21 14:30:50 +0000

    Add first stage

---
+++ zoom.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    var modal = document.getElementById('myModal');
+
+    var modalImg = document.getElementById("img01");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+  
+}
